81e21a6a8f89a939fc52cc27185c0dfdb456b8cb
Norman Isaza <isaza00@hotmail.com>
2020-02-05 18:58:51 +0000

    exercises 4 5 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+
+/*
+ * Prints Programming is like building a multilingual puzzle with puts
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
