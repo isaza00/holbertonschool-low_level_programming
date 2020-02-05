5e6b05cbb4d38e71af3a6ab86916a66afa28e994
Norman Isaza <isaza00@hotmail.com>
2020-02-05 17:56:28 +0000

    first four ex

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * Prints Programming is like building a multilingual puzzle with puts
+ */
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
