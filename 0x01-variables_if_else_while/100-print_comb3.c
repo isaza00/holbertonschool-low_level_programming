8b888e36d17eaa8f8c18600e527eb242160fb70f
Norman Isaza <isaza00@hotmail.com>
2020-02-06 20:50:22 +0000

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int inc = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = inc; j <= 9; j++)
+		{
+			if (!(i == j))
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (!(i == 9 && j == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		inc++;
+	}
+	putchar('\n');
+	return (0);
+}
