9f2d5f0ca3b6aa5d6d500a0b47220139913e8ac1
Norman Isaza <isaza00@hotmail.com>
2020-02-07 01:16:05 +0000

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for(m = 0; m <= 9; m++)
+				{
+					if (m > j && k >= i)
+					{
+						putchar(i + '0');
+						putchar(j + '0');
+						putchar(' ');
+						putchar(k + '0');
+						putchar(m + '0');
+						if (!(i == 9 && j == 8 && k == 9 && m == 9))
+						{						
+							putchar(',');
+							putchar(' ');
+						}
+					} 
+				}
+			}			
+		}
+	}
+	putchar('\n');
+	return (0);
+}
