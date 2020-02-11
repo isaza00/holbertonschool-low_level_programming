31846eca20802c11bd896d5aa221e22406e1c68f
Norman Isaza <isaza00@hotmail.com>
2020-02-11 23:31:20 +0000

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	long int sum_ant = 1;
+	long int sum_new = 2;
+	long int new = 2;
+	long int total = 0;
+
+	for (i = 0; i <= 47; i++)
+	{	
+		
+		new = sum_ant + sum_new;
+		if (new < 4000000)
+		{
+			if ((new % 2) == 0)
+			total += new;
+		}
+		sum_ant = sum_new;
+		sum_new = new;
+	}
+	printf("%li\n", total);
+	return (0);
+}
