8771cf4204525c6f9f3fa34db65ec92bc5857927
Norman Isaza <isaza00@hotmail.com>
2020-02-11 17:32:42 +0000

    fix 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,11 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
-		last_digit = n * -1;
-	else
-		last_digit = n;
 	last_digit %= 10;
+	if (last_digit < 0)
+		last_digit = last_digit * (-1);
+	else
+		last_digit = last_digit;
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
