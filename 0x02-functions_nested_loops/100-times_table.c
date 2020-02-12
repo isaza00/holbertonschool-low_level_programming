130892f0c37b214e3f8d4d367bcad04c6c83e12c
Norman Isaza <isaza00@hotmail.com>
2020-02-12 03:34:11 +0000

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,9 +17,9 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			res = i * j;
-			if ((res == 0) && (j == 0))
+			if (j == 0)
 				_putchar(res + '0');
-			if (res < 10)
+			else if ((res < 10) && (j != 0))
 			{
 				_putchar(' ');
 				_putchar(' ');
