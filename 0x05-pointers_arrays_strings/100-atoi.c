e69a755edafc8e8d26be1bd3800e941b3937a227
Norman Isaza <isaza00@hotmail.com>
2020-02-18 21:16:55 +0000

    100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _atoi - isdigit
+ * @s: char
+ * Return: prints alphabet
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int menos = 0;
+	int number = 0;
+	int found = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (found == 1 && (!(s[i] >= '0' && s[i] <= '9')))
+			break;
+		if (s[i] == '-')
+			menos++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			number = (number * 10) + s[i] - '0';
+			found = 1;
+		}
+	}
+	if (menos % 2 != 0)
+		number = number * -1;
+	return (number);
+}
