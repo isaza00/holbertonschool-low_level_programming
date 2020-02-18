3bcf3491b7899c3888c36851f89e5331728f65be
Norman Isaza <isaza00@hotmail.com>
2020-02-18 19:04:05 +0000

    fix 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,12 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i <= (n - 1); i++)
+	{
 		if (i != (n - 1))
 			printf("%i, ", a[i]);
 		else
-			printf("%i\n", a[i]);
+			printf("%i", a[i]);
+	}
+	printf("\n");
 }
