ba85d16dd65c350dfe2b371225b24339235e0917
Norman Isaza <isaza00@hotmail.com>
2020-02-19 02:52:35 +0000

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,23 +7,20 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int menos = -1;
+	int menos = 1;
 	int number = 0;
-	int found = 0;
 	int salida = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; (!(s[i] >= '0' && s[i] <= '9')); i++)
 	{
-		if (found == 1 && (!(s[i] >= '0' && s[i] <= '9')))
-			break;
 		if (s[i] == '-')
 			menos *= -1;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			number = (number * 10) + s[i] - '0';
-			found = 1;
-		}
 	}
-	salida = number * menos * -1;
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		number = (number * 10) + s[i] - '0';
+		i++;
+	}
+	salida = number * menos;
 	return (salida);
 }
