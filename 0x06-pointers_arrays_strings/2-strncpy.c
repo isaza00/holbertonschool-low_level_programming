bc3b955f07cc4ce948856513b781006cb7807fd8
Norman Isaza <isaza00@hotmail.com>
2020-02-21 00:29:49 +0000

    0x06

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,13 +8,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	size_t i;
 
-	for (i = 0; (src[i] != '\0') && (i < n);  i++)
-	{
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	}
-	for (; (i < n; i++)
+	for ( ; i < n; i++)
 		dest[i] = '\0';
 	return (dest);
 }
