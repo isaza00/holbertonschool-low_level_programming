0d4197f1bbe9a094514be3041e7145a55547c677
Norman Isaza <isaza00@hotmail.com>
2020-02-21 04:57:48 +0000

    102

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,86 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * infinite_add - isdigit
+ * @n1: integer
+ * @n2: integer
+ * @r: integer
+ * @size_r: integer
+ * Return: prints alphabet
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0;
+	int len1 = 0;
+	int len2 = 0;
+	int l1 = 0;
+	int l2 = 0;
+	int suma = 0;
+	int llevo = 0;
+	int s1 = 0;
+	int s2 = 0;
+
+	void rev_string(char *s);
+	
+	for (i = 0; n1[i] != '\0'; i++)
+		len1++;
+	for (i = 0; n2[i] != '\0'; i++)
+		len2++;
+	if (len1 >= len2)
+	{
+		l1 = len1 + 1;
+		l2 = len2 + 1;
+	}
+	else
+	{	
+		l1 = len2 + 1;
+		l2 = len1 + 1;
+	}
+	
+	for (i = 0; (i <= (l1 - 2)) || (llevo == 1); i++)
+	{
+		if ((l2 - 2 - i) >= 0)
+    			s2 = n2[l2 - 2 - i] - '0';
+    		else 
+    			s2 = 0;
+    		if ((l1 - 2 - i) >= 0)
+    			s1 = n1[l1 - 2 - i] - '0';
+    		else
+    			s1 = 0;
+    		suma = s1 + s2 + llevo;
+    		llevo = suma / 10;
+		if (i < (size_r - 1))
+		{
+	    		r[i] = (suma % 10) + '0';
+		}
+		else
+		{
+			return (0);
+		} 
+	}
+    	r[i] = '\0';
+	rev_string(r);
+	return (r);
+}
+/**
+ * rev_string - isdigit
+ * @s
+ * Return: prints alphabet
+ */
+void rev_string(char *s)
+{
+	int i;
+	int len = 0;
+	char save = 0;
+	int final = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		len++;
+	final = len - 1;
+	for (i = final; i >= (final - i); i--)
+	{
+		save = s[(final - i)];
+		s[(final - i)] = s[i];
+		s[i] = save;
+	}
+}
