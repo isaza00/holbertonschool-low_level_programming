ac9412033184817a1e1def0a5ee3c90062e7619e
isaza00 <isaza00@hotmail.com>
2020-02-24 14:58:50 -0500

    fix 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; s[i] != ','; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
@@ -20,6 +20,8 @@ unsigned int _strspn(char *s, char *accept)
 			{
 				inc++;
 			}
+			if (s[i] == ',')
+				break;
 		}
 	}
 	return (inc);
