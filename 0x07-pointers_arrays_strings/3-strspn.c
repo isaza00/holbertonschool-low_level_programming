60c3dcfee71c3afa23bb1e976a94c5a903ad58f8
isaza00 <isaza00@hotmail.com>
2020-02-24 15:08:46 -0500

    fix 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,20 +8,20 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int inc = 0;
+	unsigned int inc = 0;
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; accept[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; s[j] != '\0'; j++)
 		{
-			if (accept[j] == s[i])
+			if (s[j] == ' ')
+				break;
+			if (accept[i] == s[j])
 			{
 				inc++;
 			}
-			if (s[i] == ' ')
-				break;
 		}
 	}
 	return (inc);
