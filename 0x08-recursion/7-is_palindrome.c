f5c604ac08e780dab457558295dd10256c070fad
isaza00 <isaza00@hotmail.com>
2020-02-26 20:47:45 -0500

    7

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,21 +1,30 @@
 #include "holberton.h"
 /**
- * is_palindrome - isdigit
+ * is_mine - isdigit
  * @s: integer
+ * @len: integer
+ * @par: integer
  * Return: prints alphabet
  */
-int is_mine(char *s, int len)
+int is_mine(char *s, int len, int par)
 {
 	int i = 0;
 
 	if (s[i] != s[len])
 		return (0);
-	i++;
-	len--;
+	if (par)
+		i++;
 	if (i == len)
 		return (1);
-	return(is_mine(s, len));
+	if (par == 0)
+		i++;
+	return (is_mine(&s[i], len - 2, par));
 }
+/**
+ * _strlen_recursion - isdigit
+ * @s: integer
+ * Return: prints alphabet
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
@@ -24,15 +33,23 @@ int _strlen_recursion(char *s)
 	}
 	return (1 + _strlen_recursion(s + 1));
 }
+/**
+ * is_palindrome - isdigit
+ * @s: integer
+ * Return: prints alphabet
+ */
 int is_palindrome(char *s)
 {
 	int len = 0;
-	
+	int par = 0;
+
 	len = _strlen_recursion(s);
+	par = (len % 2 == 0) ? 1 : 0;
+
 	if (s[0] == '\0')
 	{
 		return (1);
 	}
-	return (is_mine(s, len - 1));
+	return (is_mine(s, len - 1, par));
 }
 
