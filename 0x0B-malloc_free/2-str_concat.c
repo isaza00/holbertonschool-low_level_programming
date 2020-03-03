3a76acfa70ea6a06f7a4d3ea9c75dcfe9b804a5d
isaza00 <isaza00@hotmail.com>
2020-03-03 14:38:17 -0500

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,10 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0;
 	int len2 = 0;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		concatenate[0] = "";
-		return (concatenate);
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (len1 = 0; s1[len1]; len1++)
 	{}
 	for (len2 = 0; s2[len2]; len2++)
@@ -26,25 +25,12 @@ char *str_concat(char *s1, char *s2)
 	concatenate = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (concatenate == NULL)
 		return (NULL);
-	if (s1 != NULL && s2 != NULL)
+	for (i = 0; i < len1; i++)
+		concatenate[i] = s1[i];
+	for (j = 0; j < len2; j++)
 	{
-		for (i = 0; i < len1; i++)
-			concatenate[i] = s1[i];
-		for (j = 0; j < len2; j++)
-		{
-			concatenate[i] = s2[j];
-			i++;
-		}
-	}
-	else if (s2 == NULL)
-	{
-		for (i = 0; i < len1; i++)
-			concatenate[i] = s1[i];
-	}
-	else if (s1 == NULL)
-	{
-		for (i = 0; i < len2; i++)
-			concatenate[i] = s2[i];
+		concatenate[i] = s2[j];
+		i++;
 	}
 	concatenate[i] = '\0';
 	return (concatenate);
