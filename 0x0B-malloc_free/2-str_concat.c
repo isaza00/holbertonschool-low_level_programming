bad5badf6fbf3f8cb1a9edb0fdaa254a0d95a0e3
isaza00 <isaza00@hotmail.com>
2020-03-03 13:50:34 -0500

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,17 +14,17 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0;
 	int len2 = 0;
 
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
 	for (len1 = 0; s1[len1]; len1++)
-	{
-	}
+	{}
 	for (len2 = 0; s2[len2]; len2++)
-	{
-	}
+	{}
+	concatenate = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (concatenate == NULL)
+		return (NULL);
 	if (s1 != NULL && s2 != NULL)
 	{
-		concatenate = malloc(sizeof(char) * (len1 + len2 + 1));
-		if (concatenate == NULL)
-			return (NULL);
 		for (i = 0; i < len1; i++)
 			concatenate[i] = s1[i];
 		for (j = 0; j < len2; j++)
@@ -35,17 +35,11 @@ char *str_concat(char *s1, char *s2)
 	}
 	else if (s2 == NULL)
 	{
-		concatenate = malloc(sizeof(char) * (len1 + 1));
-		if (concatenate == NULL)
-			return (NULL);
 		for (i = 0; i < len1; i++)
 			concatenate[i] = s1[i];
 	}
 	else if (s1 == NULL)
 	{
-		concatenate = malloc(sizeof(char) * (len2 + 1));
-		if (concatenate == NULL)
-			return (NULL);
 		for (i = 0; i < len2; i++)
 			concatenate[i] = s2[i];
 	}
