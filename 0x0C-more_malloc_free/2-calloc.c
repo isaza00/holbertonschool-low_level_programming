ee46c76485472e3cd9031e998683948dab6d103e
isaza00 <isaza00@hotmail.com>
2020-03-05 13:41:16 -0500

    5

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
-		return (NULL);	
+		return (NULL);
 	for (; i < (nmemb * size); i++)
 		p[i] = 0;
 	return (p);
