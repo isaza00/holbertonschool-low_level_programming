b4dab3289a74a8dcc873916c5d7856e24d3600fe
isaza00 <isaza00@hotmail.com>
2020-03-09 14:39:57 -0500

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
-		return (NULL);	
+		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
