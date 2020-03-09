f5f1317315e761d79b2ec44080ed384f47706af0
isaza00 <isaza00@hotmail.com>
2020-03-09 16:00:19 -0500

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
 /**
  * new_dog - isdigit
@@ -10,12 +11,40 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *new_name;
+	char *new_owner;
+	int i = 0;
+	int len1 = 0;
+	int len2 = 0;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = name;
+	for (i = 0; name[i]; i++)
+		len1++;
+	for (i = 0; owner[i]; i++)
+		len1++;
+	new_name = malloc(sizeof(char) * len1 + 1);
+	if (new_name == NULL)
+	{
+		free (d);
+		return (NULL);
+	}
+	new_owner = malloc(sizeof(char) * len2 + 1);
+	if (new_owner == NULL)
+	{
+		free (new_name);
+		free (d);
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
+		new_name[i] = name[i];
+	new_name[i] = '\0';
+	for (i = 0; owner[i]; i++)
+		new_owner[i] = owner[i];
+	new_owner[i] = '\0';
+	d->name = new_name;
 	d->age = age;
-	d->owner = owner;
+	d->owner = new_owner;
 	return (d);
 }
