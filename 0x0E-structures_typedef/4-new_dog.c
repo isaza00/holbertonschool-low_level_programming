1de62e576ce947849a7431df1d4dca4c8d8d40cb
isaza00 <isaza00@hotmail.com>
2020-03-09 13:37:30 -0500

    2 y 3

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - isdigit
+ * @d: integer
+ * @name: integer
+ * @age: integer
+ * @owner: integer
+ * Return: prints alphabet
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+		if (d == NULL)
+			return;
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
