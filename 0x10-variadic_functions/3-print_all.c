0ea83ebadf2519122b88f75ff69547e3b21aa20c
isaza00 <isaza00@hotmail.com>
2020-03-15 11:36:12 -0500

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,92 @@
-#include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "variadic_functions.h"
 /**
- * print_all - isdigit
- * @format: integer
- * Return: prints alphabet
+ * print_char - prints a character
+ *
+ * @c: character to be printed
  */
-void print_all(const char *const format, ...)
+void print_char(va_list c)
 {
-	int i = 0;
-	va_list args;
-	char *s;
-
-	va_start(args, format);
-	while (format[i] != '\0')
-	{
-		switch (format[i])
-		{
-			case 'f':
-				printf("%f", va_arg(args, double));
-				if (format[i + 1] != '\0')
-					printf(", ");
-				break;
-			case 'i':
-				printf("%i", va_arg(args, int));
-				if (format[i + 1] != '\0')
-					printf(", ");
-				break;
-			case 'c':
-				printf("%c", va_arg(args, int));
-				if (format[i + 1] != '\0')
-					printf(", ");
-				break;
-			case 's':
-				s = va_arg(args, char *);
-				if (s == NULL)
-				{
-					printf("(nil)");
-					if (format[i + 1] != '\0')
-						printf(", ");
-				} else
-				{
-					printf("%s", s);
-					if (format[i + 1] != '\0')
-						printf(", ");
-				} break;
-		} i++;
-	} printf("\n");
-	va_end(args);
+	printf("%c", va_arg(c, int));
 }
 
+/**
+ * print_int - prints an integer
+ *
+ * @i: integer to be printed
+ */
+void print_int(va_list i)
+{
+	printf("%i", va_arg(i, int));
+}
+
+/**
+ * print_float - prints a float
+ *
+ * @f: float to be printed
+ */
+void print_float(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_str - prints a string
+ *
+ * @s: string to be printed
+ */
+
+void print_str(va_list s)
+{
+	char *str;
+
+	str = va_arg(s, char *);
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to function
+ */
+
+void print_all(const char *const format, ...)
+{
+	var_types anything[] = {
+	{"c", print_char},
+	{"i", print_int},
+	{"f", print_float},
+	{"s", print_str},
+	{NULL, NULL}};
+
+	unsigned int i, j;
+	char *commsp;
+	va_list args;
+
+	i = 0;
+	j = 0;
+	commsp = "";
+
+	va_start(args, format);
+
+	while ((format != NULL) && (format[i] != '\0'))
+	{
+		while (j < 4)
+		{
+			if (format[i] == *(anything[j].car))
+			{
+				printf("%s", commsp);
+				anything[j].f(args);
+				commsp = ", ";
+			}
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+	printf("\n");
+	va_end(args);
+}
