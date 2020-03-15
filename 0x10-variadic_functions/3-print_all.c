ccc4b93521a7161a03baefa03f26ad73a0c8b00f
isaza00 <isaza00@hotmail.com>
2020-03-15 11:26:49 -0500

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,33 +3,51 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * print_strings- isdigit
- * @separator: integer
- * @n: integer
+ * print_all - isdigit
+ * @format: integer
  * Return: prints alphabet
  */
 void print_all(const char *const format, ...)
 {
 	int i = 0;
-	char *string;
 	va_list args;
+	char *s;
 
-	va_start(args, n);
-
-	i = n;
-	if (n <= 0 || !separator)
-		return;
-	while (i--)
+	va_start(args, format);
+	while (format[i] != '\0')
 	{
-		string = va_arg(args, char *);
-		if (string == NULL)
-			string = "(nil)";
-		if (i != 0)
+		switch (format[i])
 		{
-			printf("%s%s", string, separator);
-		}
-		if (i == 0)
-			printf("%s\n", string);
-	}
+			case 'f':
+				printf("%f", va_arg(args, double));
+				if (format[i + 1] != '\0')
+					printf(", ");
+				break;
+			case 'i':
+				printf("%i", va_arg(args, int));
+				if (format[i + 1] != '\0')
+					printf(", ");
+				break;
+			case 'c':
+				printf("%c", va_arg(args, int));
+				if (format[i + 1] != '\0')
+					printf(", ");
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+					if (format[i + 1] != '\0')
+						printf(", ");
+				} else
+				{
+					printf("%s", s);
+					if (format[i + 1] != '\0')
+						printf(", ");
+				} break;
+		} i++;
+	} printf("\n");
 	va_end(args);
 }
+
