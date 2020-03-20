6233b6f530c1fa6a59c9135af02cbd331e935878
isaza00 <isaza00@hotmail.com>
2020-03-19 20:52:04 -0500

    4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,23 +1,24 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "lists.h"
+
 /**
  * free_list - isdigit
  * @head: integer
  * Return: prints alphabet
  */
+
 void free_list(list_t *head)
 {
-	list_t *temporal;
+	list_t *aux;
 
-	temporal = head;
-	while (temporal != NULL)
+	aux = head;
+	while (aux != NULL)
 	{
-		if (temporal->str != NULL)
-			free(temporal->str);
-		free(temporal);
-		temporal = temporal->next;
+		if (aux->str != NULL)
+			free(aux->str);
+		free(aux);
+		aux = aux->next;
 	}
 }
+
 
