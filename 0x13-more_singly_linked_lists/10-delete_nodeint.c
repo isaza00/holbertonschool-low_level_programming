71432a73597c6be709c17479a985244b07055445
isaza00 <isaza00@hotmail.com>
2020-03-24 10:34:57 -0500

    if

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *temp, *previous;
+	listint_t *temp, *previous = NULL;
 
 	if (!*head || !head)
 		return (-1);
