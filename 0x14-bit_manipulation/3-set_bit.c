75bcd726fa59dad8fce91228da012e747fbbe7bf
isaza00 <isaza00@hotmail.com>
2020-03-26 20:40:55 -0500

    3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,28 +9,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int i = 0;
-	unsigned long int bit = 1, m = 0;
+	unsigned long int bit = 1;
 
 	if (index > 31)
 		return (-1);
-	if (index == 0)
-	{
-		*n |= bit;
-		return (1);
-	}
-	m = *n;
-	while (m > 0)
-	{
-		m = m >> 1;
-		bit = bit << 1;
-		i++;
-		if (i == index)
-		{
-			*n |= bit;
-			return (1);
-		}
-	}
-	return (-1);
+	bit = bit << index;
+	*n |= bit;
+	return (1);
 }
 
