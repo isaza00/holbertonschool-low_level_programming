c6d4d5d80bbaeddcc1bd23a0f2cac45c0b7caf40
isaza00 <isaza00@hotmail.com>
2020-05-02 17:35:52 -0500

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,11 +20,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		number_nodes += 1;
 		temp = temp->next;
 	}
-	if (idx == 0)
-		return (add_dnodeint(h, n));
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	if (idx == number_nodes)
+		return (add_dnodeint_end(h, n));
 	new->n = n;
 	temp = *h;
 	while (temp)
--- 0x17-doubly_linked_lists/7-main.c
+++ 0x17-doubly_linked_lists/7-main.c
@@ -13,11 +13,22 @@ int main(void)
     dlistint_t *head;
 
     head = NULL;
-    
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    printf("lista al derecho: \n");
     print_dlistint(head);
     printf("-----------------\n");
-    insert_dnodeint_at_index(&head, 0, 4096);
+    insert_dnodeint_at_index(&head, 8, 4096);
+    printf("lista al derecho despues de insertar nodo: \n");
     print_dlistint(head);
+    printf("lista al reves despues de insertar nodo: \n");
+    print_rev_dlistint(head);
     free_dlistint(head);
     head = NULL;
     return (EXIT_SUCCESS);
