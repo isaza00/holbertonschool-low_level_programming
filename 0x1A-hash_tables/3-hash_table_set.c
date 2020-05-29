5a304bb16e13c1335385e7ff3fa5794b0342c772
Norman <isaza00@hotmail.com>
2020-05-29 12:59:40 -0500

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,6 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || strcmp(key, "") == 0 || !key || !value)
 		return (0);
+	index = key_index((unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
