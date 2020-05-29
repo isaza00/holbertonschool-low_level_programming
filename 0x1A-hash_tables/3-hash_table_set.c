4e0b9f7fce0349d39ade0261635239007adc574a
Norman <isaza00@hotmail.com>
2020-05-29 13:00:54 -0500

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,8 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		new_node->next = NULL;
 		ht->array[index] = new_node;
-		return (1);
-	}
+		return (1); }
 	temp = ht->array[index];
 	while (temp)
 	{
