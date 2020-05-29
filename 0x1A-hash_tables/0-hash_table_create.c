1063a0abd63a59da56a3acd10a2bd6949329b84a
Norman <isaza00@hotmail.com>
2020-05-29 12:46:07 -0500

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,19 +7,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
+	hash_table_t *ht;
 	unsigned long int i = 0;
 
-	new_table = malloc(sizeof(hash_table_t));
-	if (!new_table)
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
 		return (NULL);
-	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (!new_table->array)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (!ht->array)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
-		new_table->array[i] = NULL;
-	return (new_table);
+		ht->array[i] = NULL;
+	return (ht);
 }
