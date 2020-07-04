d63a60b6c9e7e586421623c75560f35a4af0a56c
Norman <isaza00@hotmail.com>
2020-07-03 23:09:57 -0500

    5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,7 +8,9 @@ def island_perimeter(grid):
     if grid:
         for i in range(len(grid)):
             for j in range(len(grid[i])):
-                if grid[i][j] != 1:
+                if grid[i][j] == 1:
+                    pass
+                else:
                     if i > 0:
                         if grid[i-1][j] == 1:
                             cont += 1
