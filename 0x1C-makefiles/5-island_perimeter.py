79197aa282c7fee03fe25b7583b83b063d6a4bba
Norman <isaza00@hotmail.com>
2020-07-03 23:21:52 -0500

    5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,20 +7,19 @@ def island_perimeter(grid):
     cont = 0
     height = len(grid)
     width = len(grid[0])
-    if grid:
-        for i in range(height):
-            for j in range(width):
-                if grid[i][j] == 0:
-                    if i > 0:
-                        if grid[i-1][j] == 1:
-                            cont += 1
-                    if j > 0:
-                        if grid[i][j-1] == 1:
-                            cont += 1
-                    if j < width - 1:
-                        if grid[i][j+1] == 1:
-                            cont += 1
-                    if i < height - 1:
-                        if grid[i+1][j] == 1:
-                            cont += 1
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 0:
+                if i > 0:
+                    if grid[i-1][j] == 1:
+                        cont += 1
+                if j > 0:
+                    if grid[i][j-1] == 1:
+                        cont += 1
+                if j < width - 1:
+                    if grid[i][j+1] == 1:
+                        cont += 1
+                if i < height - 1:
+                    if grid[i+1][j] == 1:
+                        cont += 1
     return cont
